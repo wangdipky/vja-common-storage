49748f785c3fd68df3847f574a9706cb1c2a581c
wangdipky <vangv4v@gmail.com>
2026-01-18 23:36:33 +0700

    [Quang] init project

---
+++ src/main/java/vn/com/vja/VjaCommonStorageApplication.java
@@ -0,0 +1,13 @@
+package vn.com.vja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VjaCommonStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VjaCommonStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vn/com/vja/VjaCommonStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.com.vja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VjaCommonStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
